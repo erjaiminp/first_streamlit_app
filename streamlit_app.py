8b55c3aa86b65ded70a158f93e18e9396dabc859
Jaimin Patel <134231362+erjaiminp@users.noreply.github.com>
2023-07-06 15:26:23 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
-streamlit.title('Hello World')
-streamlit.text('This is my First Program')
+streamlit.title('My New Healthy Diet Plan')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
